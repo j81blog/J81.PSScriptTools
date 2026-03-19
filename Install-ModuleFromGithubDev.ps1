3b40ff8532d76a603f1af5358027679002e9b7cb
John Billekens Consultancy <john@j81.nl>
2026-03-19 18:58:03 +0100

    Refactor Install-ModuleFromGithubDev.ps1 and Install-ModuleFromGithubMain.ps1 to streamline variable assignments and improve execution policy handling

--- Install-ModuleFromGithubDev.ps1
+++ Install-ModuleFromGithubDev.ps1
@@ -1,30 +1,28 @@
-﻿$RemoteBranch = "dev"
-$Uri = 'https://github.com/j81blog/J81.PSScriptTools'
-$ModuleName = 'J81.PSScriptTools'
+﻿#Requires -Version 5.1
 
-#Requires -Version 5.1
-
+# set the user module path based on edition and platform
 if ($PSVersionTable.PSEdition -eq 'Desktop') {
-    $InstallPath = [System.IO.Path]::Combine(([Environment]::GetFolderPath('MyDocuments')), 'WindowsPowerShell\Modules')
+    $installpath = Join-Path ([Environment]::GetFolderPath('MyDocuments')) 'WindowsPowerShell\Modules'
 } elseif ($IsWindows) {
-    $InstallPath = [System.IO.Path]::Combine(([Environment]::GetFolderPath('MyDocuments')), 'PowerShell\Modules')
+    $installpath = Join-Path ([Environment]::GetFolderPath('MyDocuments')) 'PowerShell\Modules'
 } else {
-    $InstallPath = [System.IO.Path]::Combine($env:HOME, '.local/share/powershell/Modules')
+    $installpath = Join-Path $env:HOME '.local/share/powershell/Modules'
 }
-Write-Verbose "InstallPath: $InstallPath"
 
-$ExecutionPolicy = Get-ExecutionPolicy
-if (('PSEdition' -notin $PSVersionTable.Keys -or $PSVersionTable.PSEdition -eq 'Desktop' -or $IsWindows) -and ($ExecutionPolicy -notin 'Unrestricted', 'RemoteSigned', 'Bypass')) {
-    Write-Host "Setting process execution policy to RemoteSigned" -ForegroundColor Cyan
-    Set-ExecutionPolicy RemoteSigned -Scope Process -Force
-} else {
-    Write-Host "Current execution policy: $ExecutionPolicy" -ForegroundColor Yellow
+# deal with execution policy on Windows
+if (('PSEdition' -notin $PSVersionTable.Keys -or
+        $PSVersionTable.PSEdition -eq 'Desktop' -or
+        $IsWindows) -and
+    (Get-ExecutionPolicy) -notin 'Unrestricted', 'RemoteSigned', 'Bypass') {
+    Write-Host "Setting user execution policy to RemoteSigned" -ForegroundColor Cyan
+    Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 }
 
 if (-not (Test-Path -Path $InstallPath)) {
     Write-Host "Creating module path: $InstallPath" -ForegroundColor Cyan
     New-Item -ItemType Directory -Force -Path $InstallPath | Out-Null
 }
+
 $ScriptPath = if ($PSScriptRoot) {
     $PSScriptRoot
 } elseif ($MyInvocation.MyCommand.Path) {
@@ -35,6 +33,11 @@ $ScriptPath = if ($PSScriptRoot) {
 Write-Verbose "ScriptPath: $ScriptPath"
 
 if ([String]::IsNullOrWhiteSpace($ScriptPath)) {
+    $Uri = 'https://github.com/j81blog/J81.PSScriptTools'
+
+    if ([String]::IsNullOrWhiteSpace($remoteBranch)) {
+        $remoteBranch = 'dev'
+    }
 
     # GitHub now requires TLS 1.2
     # https://blog.github.com/2018-02-23-weak-cryptographic-standards-removed/
@@ -45,51 +48,36 @@ if ([String]::IsNullOrWhiteSpace($ScriptPath)) {
     }
 
     $HostUrl, $Owner, $Repo = $Uri.TrimStart('https://') -split ('/')
-    $Url = 'https://{0}/{1}/{2}/archive/refs/heads/{3}.zip' -f $HostUrl, $Owner, $Repo, $RemoteBranch
+    $Url = 'https://{0}/{1}/{2}/archive/refs/heads/{3}.zip' -f $HostUrl, $Owner, $Repo, $remoteBranch
     Write-Verbose "Url: $Url"
 
-    Write-Host "Downloading latest version of $ModuleName from $Url" -ForegroundColor Cyan
-    $File = [System.IO.Path]::Combine([system.io.path]::GetTempPath(), "$ModuleName.zip")
+    Write-Host "Downloading latest version of $($ModuleName) from $url" -ForegroundColor Cyan
+    $file = Join-Path -Path ([system.io.path]::GetTempPath()) -ChildPath "$($ModuleName).zip"
     $webclient = New-Object System.Net.WebClient
     try {
-        $webclient.DownloadFile($Url, $File)
-        Write-Verbose "File downloaded: $File"
+        $webclient.DownloadFile($url, $file)
     } catch {
-        Write-Host "Failed to download the file from $Url, Error $($_.Exception.Message)" -ForegroundColor Red
-        throw $_
+        throw
     }
-    Write-Host "File saved to $File" -ForegroundColor Green
+    Write-Host "File saved to $file" -ForegroundColor Green
 
-    Write-Host "Expanding $ModuleName.zip to $($InstallPath)" -ForegroundColor Cyan
-    Expand-Archive -Path $File -DestinationPath $InstallPath
+    # extract the zip
+    Write-Host "Expanding $($ModuleName).zip to $($InstallPath)" -ForegroundColor Cyan
+    Expand-Archive $file -DestinationPath $installpath
 
-    #Extract module version from module manifest
-    $ModuleManifest = Get-ChildItem -Path $InstallPath -Filter "$ModuleName*.psd1" -Recurse | Select-Object -First 1
-    if ($null -eq $ModuleManifest) {
-        Write-Host "Module manifest not found in $($InstallPath)" -ForegroundColor Red
-        throw "Module manifest not found"
-    } else {
-        $ModuleInfo = Import-PowerShellDataFile -Path $ModuleManifest.FullName
-        $ModuleVersion = $ModuleInfo.ModuleVersion
-        Write-Host "Module version: $($ModuleVersion)" -ForegroundColor Green
-    }
-
-    if (Test-Path -Path "$($InstallPath)\$($ModuleName)") {
-        Write-Host "Removing any old copy" -ForegroundColor Cyan
-        Remove-Item -Path "$($InstallPath)\$($ModuleName)" -Recurse -Force -ErrorAction Continue
-    }
+    Write-Host "Removing any old copy" -ForegroundColor Cyan
+    Remove-Item "$installpath\$($ModuleName)" -Recurse -Force -EA Ignore
     Write-Host "Renaming folder" -ForegroundColor Cyan
-    Copy-Item -Path "$($InstallPath)\$($ModuleName)-$($RemoteBranch)\$($ModuleName)" -Destination $InstallPath -Recurse -Force -ErrorAction Continue
-    Remove-Item -Path "$($InstallPath)\$($ModuleName)-$($RemoteBranch)" -Recurse -Force
-    Write-Host "Importing module from local path, force reloading" -ForegroundColor Cyan
-    Import-Module -Name $ModuleName -Force
+    Copy-Item "$($installpath)\$($ModuleName)-$($remoteBranch)\$($ModuleName)" $installpath -Recurse -Force -EA Continue
+    Remove-Item "$($installpath)\$($ModuleName)-$($remoteBranch)" -Recurse -Force
+    Import-Module -Name $($ModuleName) -Force
 } else {
-    Write-Host "Running locally from $($PSScriptRoot)" -ForegroundColor Cyan
-    Remove-Item -Path "$($InstallPath)\$($ModuleName)" -Recurse -Force -ErrorAction Ignore
-    Copy-Item -Path "$($PSScriptRoot)\$($ModuleName)" -Destination $InstallPath -Recurse -Force -ErrorAction Continue
-    Write-Host "Importing module from local path, force reloading" -ForegroundColor Cyan
-    Import-Module -Name $ModuleName -Force
+    # running locally
+    Remove-Item "$installpath\$($ModuleName)" -Recurse -Force -EA Ignore
+    Copy-Item "$PSScriptRoot\$($ModuleName)" $installpath -Recurse -Force -EA Continue
+    # force re-load the module (assuming you're editing locally and want to see changes)
+    Import-Module -Name $($ModuleName) -Force
 }
 Write-Host 'Module has been installed' -ForegroundColor Green
 
-Get-Command -Module $ModuleName | Format-Table -AutoSize
+Get-Command -Module $($ModuleName)
